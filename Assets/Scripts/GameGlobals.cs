25ba15d09945d3e3ed136458ba0717ce36aaf853
Tyler Main <tylermain3049@gmail.com>
2017-03-10 19:11:58 -0500

    uploading assets : models + music 1/2
    model and texture assets
    music player
    rhythm class
    general purpose timer class
    prefabs for units with attached models, and for music player with attached rhythm class

---
+++ Assets/Scripts/GameGlobals.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGlobals : MonoBehaviour {
+
+	public static string playerTag = "Player";
+	public static string enemyTag = "Enemy";
+	public static string terrainTag = "Terrain";
+}
---
+++ Assets/Scripts/Rhythm.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rhythm : MonoBehaviour {
+
+	private BasicTimer songTimer;
+	public float beatTime;
+	public float startOffset;
+	private bool upBeat = false;
+
+	void Awake(){
+		songTimer = BasicTimer.SetupTimer (gameObject, beatTime, true, true, startOffset);
+	}
+
+	void Update(){
+		if (songTimer.HasCycled ()) {
+			upBeat = !upBeat;
+		}
+		//Debug.Log ("upBeat = " + upBeat);
+	}
+
+	public bool IsOnUpBeat(){
+		return upBeat;
+	}
+}
