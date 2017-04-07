ca0194ae2217dac22183b1cb066bddbee9d92951
Shalune <TylerMain@cmail.carleton.ca>
2017-04-07 18:25:38 -0400

    adjusted terrain, and unit textures'

---
+++ Assets/Scripts/UnitComponents/ArmyColorer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ArmyColorer : MonoBehaviour {
+
+	public GameObject rendererAttachTo;
+	public Material enemyMat;
+
+	void Awake(){
+		// if owner is enemy and it has swappable textures, do so on awake
+		if (GetComponentInParent<Unit> ().TeamNumber != 0 && rendererAttachTo != null) {
+			rendererAttachTo.GetComponent<Renderer>().material = enemyMat;
+		}
+	}
+}
